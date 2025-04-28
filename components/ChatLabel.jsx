642ac9e16d4be1b70e87be1887a1446c076a3744
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-04-28 11:41:55 +0600

    Chat Label Added

---
+++ components/ChatLabel.jsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { assets } from '@/assets/assets';
+import Image from 'next/image';
+import React from 'react';
+
+const ChatLabel = ({ openMenu, setOpenMenu, id }) => {
+  const toggleMenu = (e) => {
+    e.stopPropagation();
+    setOpenMenu((prev) =>
+      prev.id === id && prev.open
+        ? { id: 0, open: false }
+        : { id, open: true }
+    );
+  };
+
+  return (
+    <div
+      className="flex items-center justify-between p-2 text-white/80 hover:bg-white/10 rounded-lg text-sm group cursor-pointer"
+    >
+      <p className="group-hover:max-w-5/6 truncate">Chat Name Here</p>
+      <div
+        onClick={toggleMenu}
+        className="relative flex items-center justify-center h-6 w-6 aspect-square hover:bg-black/80 rounded-lg"
+      >
+        <Image
+          src={assets.three_dots}
+          alt="Options"
+          className="w-4"
+        />
+        {openMenu.id === id && openMenu.open && (
+          <div className="absolute -right-36 top-6 bg-gray-700 rounded-xl w-max p-2">
+            <div className="flex items-center gap-3 hover:bg-white/10 px-3 py-2 rounded-lg cursor-pointer">
+              <Image src={assets.pencil_icon} alt="Rename" className="w-4" />
+              <p>Rename</p>
+            </div>
+            <div className="flex items-center gap-3 hover:bg-white/10 px-3 py-2 rounded-lg cursor-pointer">
+              <Image src={assets.delete_icon} alt="Delete" className="w-4" />
+              <p>Delete</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ChatLabel;
